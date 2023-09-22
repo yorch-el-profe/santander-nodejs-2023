9fb1f6dc8e2a73c86ef56a628aef06f02fbec653
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-09-21 20:16:59 -0600

    Agregando el CRUD de publicaciones

--- sesion-03-rest/00-bedu-plus/controllers/post.js
+++ sesion-03-rest/00-bedu-plus/controllers/post.js
@@ -1,8 +1,39 @@
-exports.getPosts = function (request, response) {
-	response.json([]);
+const {
+	findAll,
+	findById,
+	insert,
+	deleteById,
+	update,
+} = require("../services/post");
+
+exports.getPosts = async function (request, response) {
+	const posts = await findAll();
+	response.json(posts);
 };
 
-exports.createPost = function (request, response) {
-	console.log(request.body);
-	response.send("Articulo creado");
+exports.getPost = async function (request, response) {
+	const { id } = request.params;
+	const post = await findById(id);
+	response.json(post);
+};
+
+exports.createPost = async function (request, response) {
+	const { content } = request.body;
+	// TODO: Implementar el userId del token JWT
+	const post = await insert({ content, userId: 1 });
+	response.json(post);
+};
+
+exports.updatePost = async function (request, response) {
+	const { content } = request.body;
+	const { id } = request.params;
+
+	await update(id, { content });
+	response.send("Publicación actualizada");
+};
+
+exports.deletePost = async function (request, response) {
+	const { id } = request.params;
+	await deleteById(id);
+	response.send("Publicacion borrada");
 };
--- sesion-03-rest/00-bedu-plus/routers/post.js
+++ sesion-03-rest/00-bedu-plus/routers/post.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts, createPost } = require("../controllers/post");
+const {
+	getPosts,
+	createPost,
+	getPost,
+	deletePost,
+	updatePost,
+} = require("../controllers/post");
 
 router.get("/getPosts", getPosts);
+router.get("/getPost/:id", getPost);
 router.get("/createPost", createPost);
+router.get("/updatePost/:id", updatePost);
+router.get("/deletePost/:id", deletePost);
 
 module.exports = router;
