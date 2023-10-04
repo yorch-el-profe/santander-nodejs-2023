cea3587c8e3d6ee32b44b6c4d9a1ea8e0688c875
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-10-03 21:59:28 -0600

    Sesion 06: Introducción a los mocks

---
+++ sesion-06-unit-testing/02-mocks/convert-to-objects.js
@@ -0,0 +1,31 @@
+const fs = require("fs/promises");
+
+async function convertToObjects() {
+	const text = await fs.readFile("./dataset.txt", "utf-8");
+
+	// La manera chafa (Junior)
+	const lines = text.split("\n");
+	const result = [];
+
+	for (let i = 0; i < lines.length; i++) {
+		const parts = lines[i].split("|");
+		const person = {
+			id: parts[0],
+			name: parts[1],
+			age: parts[2],
+			email: parts[3],
+		};
+		result.push(person);
+	}
+
+	return result;
+}
+
+// Versión Senior
+/*const convertToObject = async () =>
+	(await fs.readFile("./dataset.txt", "utf-8")).split("\n").map((line) => {
+		const [id, name, age, email] = line.split("|");
+		return { id, name, age, email };
+	});*/
+
+module.exports = convertToObjects;
---
+++ sesion-06-unit-testing/02-mocks/convert-to-objects.test.js
@@ -0,0 +1,14 @@
+const { convertToObjects } = require("./convert-to-objects");
+
+/**
+ * Unit Testing: Probar piezas de código aisladas, quitando cualquier dependencia externa.
+ *
+ * Integration Testing: Pruebas de varios componentes en conjunto. Es posible seguir quitando
+ * algunas dependencias externas.
+ *
+ * E2E (End-to-end) Testing: Flujos completos. Es posible seguir quitando algunas dependencias externas
+ * pero NO ES RECOMENDABLE :D
+ */
+describe("Convert To Objects Unit Test", () => {
+	describe("convertToObjects() test", () => {});
+});
